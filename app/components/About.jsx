cb5e081782f6b5af446b6cdf37adaae1a7f71f2d
Varun Babu Pozhath <varunbabu008@gmail.com>
2016-11-28 23:20:48 +0530

    update about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,11 @@ var React = require('react');
 
 var About = (props) =>{
    return(
-      <h3>About component</h3>
+    <div>
+      <h3>About</h3>
+      <p>Welcome to About page</p>
+    </div>
+      
       );
 }
 
